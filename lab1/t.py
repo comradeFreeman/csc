73dc2a32df00beeff30ff422c08a3f5886a19eb5
Comrade Freeman <mister.siroejkin@yandex.ua>
2020-03-11 10:16:26 +0200

    fix leak

--- lab1/t.py
+++
@@ -1,8 +0,0 @@
-res = {}
-# (str(bin(int(a)))[-10:-7])
-with open('codes.txt', 'r') as f:
-	for line in f.readlines():
-		res[line] = str(bin(int(line)))[-3:]
-for key, val in res.items():
-	if val == '000' or val == '001' or val == '101':
-		print(key)
